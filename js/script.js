22f4beed8d5bfab3ee3e26cda43da1b38e92902a
BetterCallAle <dap22091998@gmail.com>
2022-10-25 13:54:28 +0200

    Gabibbo now will say 'hello'

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
-            prova: "ciao"
+            userName: "",
+            greetingsMessage: "",
+            image: "",
+            alt: ""
+        }
+    },
+    methods: {
+        stampGreetings: function(){
+            this.greetingsMessage = `BEEEELANDIIII! Ciao, ${this.userName}!`
+            this.image = "img/gabibbo.jpg"
+            this.alt = "Immagine del Gabibbo"
+            this.userName = ""
         }
     }
 }).mount("#app");
